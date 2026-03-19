3833bc3920aa9521c995574fa18bb190470ad83c
RubyTso <kangjungtso@gmail.com>
2026-03-20 01:19:50 +0800

    fix: 使用預設 Sanity structure

--- sanity.config.ts
+++ sanity.config.ts
@@ -9,16 +9,7 @@ export default defineConfig({
   projectId: sanityConfig.projectId,
   dataset: sanityConfig.dataset,
   plugins: [
-    structureTool({
-      structure: (S) =>
-        S.list()
-          .title('管理後台')
-          .items([
-            S.listItem().id('activity').title('活動梯次管理').schemaType('activity').child(S.documentTypeList('activity')),
-            S.listItem().id('tutor').title('師資履歷管理').schemaType('tutor').child(S.documentTypeList('tutor')),
-            S.listItem().id('note').title('筆記商品管理').schemaType('note').child(S.documentTypeList('note')),
-          ]),
-    }),
+    structureTool(),
   ],
   schema: {
     types: schemaTypes,
