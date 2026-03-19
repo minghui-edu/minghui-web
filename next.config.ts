a468f5a9a5092b2c70ba2b2530d018d50b62bc86
RubyTso <kangjungtso@gmail.com>
2026-03-19 16:22:55 +0800

    chore: 新增 Cloudflare Pages 部署設定

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+      },
+    ],
+  },
 };
+
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
+}
 
 export default nextConfig;
